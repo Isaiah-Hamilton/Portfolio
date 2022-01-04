3f5558c8b3de98928d00b66899e300b182fbfa01
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2022-01-04 14:51:10 -0500

    init contact

---
+++ components/Contact.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="mb-16">
+      <div className="grid justify-items-start">
+        <h1 className="title-font sm:text-4xl text-3xl font-bold text-center m-auto mb-6">
+          Contact
+        </h1>
+        <p className="w-[45%] text-center m-auto mb-6">
+          I&apos;m currently looking for internship Software Engineering opportunities! If you know
+          of any positions available, if you have any questions, or if you want to get in touch,
+          please email me down below.
+        </p>
+        <Link href="mailto:isaiah7hamilton@gmail.com">
+          <a className="inline-flex py-2 px-6 m-auto focus:outline-none rounded-full text-base transition duration-500 ease-in-out shadow-xl border-2 border-brand-500 dark:bg-[#333333] hover:bg-brand-500 dark:hover:bg-brand-500 hover:text-white">
+            Contact
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next'
 import Layout from '../components/Layouts'
 import Hero from '../components/Hero'
 import Portfolio from '../components/Portfolio'
+import Contact from '../components/Contact'
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <Hero />
       <Portfolio />
+      <Contact />
     </Layout>
   )
 }
