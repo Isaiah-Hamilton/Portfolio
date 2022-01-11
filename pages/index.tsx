5cedaf6f7fafb5d2ecbd515e0e04244c05e85c21
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2022-01-10 21:31:17 -0500

    improve seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import type { NextPage } from 'next'
 import Layout from '../components/Layouts'
 import Hero from '../components/Hero'
@@ -6,11 +7,33 @@ import Contact from '../components/Contact'
 
 const Home: NextPage = () => {
   return (
-    <Layout>
-      <Hero />
-      <Portfolio />
-      <Contact />
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Isaiah Hamilton is a creative thinker and Full Stack Developer with experience in website design, front-end development, and back-end development."
+        />
+        <meta name="author" content="Isaiah Hamilton" />
+        <meta name="author" content="Isaiah-Hamilton" />
+        <meta name="author" content="Isaiah7Hamilton" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@Isaiah7Hamilton" />
+        <meta name="twitter:creator" content="@Isaiah7Hamilton" />
+        <meta name="twitter:image" content="/card.png" />
+        <meta property="og:site_name" content="Isaiah Hamilton | Full Stack Developer" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/card.png" />
+        <title>Isaiah Hamilton | Full Stack Developer</title>
+      </Head>
+      <Layout>
+        <Hero />
+        <Portfolio />
+        <Contact />
+      </Layout>
+    </>
   )
 }
 
