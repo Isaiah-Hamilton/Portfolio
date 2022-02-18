99dd2dc4d639b72ccbcd53d63848a776e3b2c708
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2022-02-17 22:40:21 -0500

    Improve theme switch (#89)
    This looks a lot better with the rest of the UI

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -47,28 +47,10 @@ function DarkModeToggle(props: Props) {
 
   return (
     <div className="flex items-center">
-      <SunIcon />
-      <button
-        type="button"
-        aria-pressed="false"
-        className={`
-                relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer 
-                transition-colors ease-in-out duration-200 focus:outline-none mx-2 ${
-                  darkMode ? 'bg-gray-500' : 'bg-gray-200'
-                }
-              `}
-        onClick={() => toggleDarkMode()}
-      >
-        <span className="sr-only">Toggle Theme</span>
-        <span
-          aria-hidden="true"
-          className={`
-                  ${darkMode ? 'translate-x-5' : 'translate-x-0'} inline-block h-5 w-5 rounded-full
-                  bg-white dark:bg-gray-700 shadow-lg transform ring-0 transition ease-in-out duration-200
-                `}
-        />
+      <button type="button" aria-pressed="false" onClick={() => toggleDarkMode()}>
+        {darkMode ? <MoonIcon /> : <SunIcon />}
+        <span className="sr-only">Theme Toggle</span>
       </button>
-      <MoonIcon />
     </div>
   )
 }
