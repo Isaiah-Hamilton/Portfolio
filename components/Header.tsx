2bb16572c55611e6f53ba5b2df9564adf21c3288
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2022-07-03 12:30:04 -0400

    chore: update seo

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta
         name="description"
-        content="Isaiah Hamilton is a creative thinker and Full Stack Developer with experience in website design, front-end development, and back-end development."
+        content="Isaiah Hamilton a creative thinker and Frontend Developer with experience in website design."
       />
       <meta name="author" content="Isaiah Hamilton" />
       <meta name="author" content="Isaiah-Hamilton" />
@@ -16,11 +16,11 @@ const Header = () => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@Isaiah7Hamilton" />
       <meta name="twitter:creator" content="@Isaiah7Hamilton" />
-      <meta name="twitter:image" content="/card.png" />
-      <meta property="og:site_name" content="Isaiah Hamilton | Full Stack Developer" />
+      <meta name="twitter:image" content="/portfolio.png" />
+      <meta property="og:site_name" content="Isaiah Hamilton | Frontend Developer" />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content="/card.png" />
-      <title>Isaiah Hamilton | Full Stack Developer</title>
+      <meta property="og:image" content="/portfolio.png" />
+      <title>Isaiah Hamilton | Frontend Developer</title>
     </Head>
   )
 }
