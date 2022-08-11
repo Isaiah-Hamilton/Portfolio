f415d66d92447cd6f208e15a2cd53b7095b07bfe
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2022-08-11 17:56:04 -0400

    update redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/dotfiles',
+        destination: 'https://github.com/isaiah-hamilton/dotfiles',
+        permanent: true,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/isaiah-hamilton',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/isaiah7hamilton',
+        permanent: true,
+      },
+    ]
+  },
 }
