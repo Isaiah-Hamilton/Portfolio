558462627572762487053b3adf44a45cab879d59
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2022-09-26 22:22:41 -0400

    update links page

--- pages/links.tsx
+++ pages/links.tsx
@@ -1,36 +1,63 @@
-import Image from 'next/image'
-import Layout from '../components/Layouts'
-import Button from '../components/Button'
+import Link from 'next/link'
+import { PinIcon } from '../components/Icons'
 import config from '../lib/config'
 
 const links = () => {
   return (
-    <Layout hideHeader hideFooter>
-      <div className="w-fit mx-auto space-y-10 text-center">
-        <div className="">
-          <Image
-            className="rounded-full"
-            src={config.links.image}
-            width="100%"
-            height="100%"
-            objectFit="cover"
-            alt="profile picture"
-          />
-        </div>
-        <div className="text-4xl font-bold">{config.links.title}</div>
-        <div className="space-y-6">
-          {config.links.links.map((link: any, i: number) => {
-            return (
-              <div key={i} className="w-fit mx-auto">
-                <Button link={link.url} externalLink>
-                  <span>{link.title}</span>
-                </Button>
+    <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12 bg-cover bg-[url('/floss.jpg')]">
+      <div className="relative sm:max-w-xl sm:mx-auto">
+        <div className="relative p-10 bg-white shadow-lg sm:rounded-3xl bg-clip-padding bg-opacity-50 border border-gray-200 card">
+          <div className="max-w-md mx-auto">
+            <div className="w-fit mx-auto">
+              <img
+                src="https://github.com/isaiah-hamilton.png"
+                width="100"
+                height="100"
+                alt="profile"
+                className="rounded-full"
+              />
+            </div>
+            <div className="py-4 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+              <div className="text-center pb-4">
+                <h1 className="font-semibold text-2xl pb-1">{config.name}</h1>
+                <span className="text-base">{config.role}</span>
+                <div className="flex items-center w-fit mx-auto">
+                  <div>
+                    <PinIcon width={'16'} height={'16'} />
+                  </div>
+                  <span className="text-sm">{config.location}</span>
+                </div>
               </div>
-            )
-          })}
+
+              <div className="flex space-x-5">
+                {config.social.map((social: any, i: number) => {
+                  return (
+                    <Link href={social.url}>
+                      <a target="_blank">
+                        <div
+                          key={i}
+                          className="p-2.5 rounded-xl"
+                          style={{ backgroundColor: social.color }}
+                        >
+                          <svg width="30" height="30" viewBox="0 0 24 24">
+                            <path
+                              fill="#FFFFFF"
+                              fill-rule="evenodd"
+                              d={social.icon}
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                        </div>
+                      </a>
+                    </Link>
+                  )
+                })}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </Layout>
+    </div>
   )
 }
 
