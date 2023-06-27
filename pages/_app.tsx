9aba848bab1a1d40e6089909b7e58e5e7e9eee96
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2023-06-27 11:31:56 -0400

    update font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Alexandria } from 'next/font/google'
+
+const alexandria = Alexandria({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={alexandria.className}>
+      <Component {...pageProps} />
+    </main >
+  )
 }
