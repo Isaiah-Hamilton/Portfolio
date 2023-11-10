00fae911cf5ab411ba3398b3ac5f64dedbd5beae
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2023-11-10 13:06:30 -0500

    update live atc image

--- lib/data.ts
+++ lib/data.ts
@@ -1,77 +1,71 @@
 export const NavLinks = [
   {
-    text: 'Home',
-    url: '/'
+    text: "Home",
+    url: "/",
   },
   {
-    text: 'Work',
-    url: '#work'
+    text: "Work",
+    url: "#work",
   },
   {
-    text: 'Experience',
-    url: '#experience'
+    text: "Experience",
+    url: "#experience",
   },
   {
-    text: 'Contact',
-    url: '#contact'
+    text: "Contact",
+    url: "#contact",
   },
-]
+];
 
 export const HeroText = [
-  [
-    'Crafting',
-    'amazing',
-  ],
-  [
-    'digital',
-    'experiences',
-    'that',
-  ],
-  [
-    'people',
-    'love',
-  ]
-]
+  ["Crafting", "amazing"],
+  ["digital", "experiences", "that"],
+  ["people", "love"],
+];
 
 export const Projects = [
   {
-    title: 'Live ATC',
-    description: "Live ATC is a web app that allows users to listen to live air traffic control communications from around the world.",
-    image: 'https://framerusercontent.com/images/qzN3xVmCxmqGR2CUWAdJWxeyYY.jpg',
-    tags: ['Data Scraping', 'UI Design', 'Prototyping'],
-    bgColor: '#050505',
-    textColor: '#ffffff'
+    title: "Live ATC",
+    description:
+      "Live ATC is a web app that allows users to listen to live air traffic control communications from around the world.",
+    image: "/liveatc.png",
+    tags: ["Data Scraping", "UI Design", "Prototyping"],
+    bgColor: "#050505",
+    textColor: "#ffffff",
   },
   {
-    title: 'Cloneify',
-    description: "Cloneify is a web app that allows users to listen to live air traffic control communications from around the world.",
-    image: 'https://framerusercontent.com/images/CWR3GLN69xNxRF87alCvZO3G9ds.jpg',
-    tags: ['UX Research', 'UI Design', 'Prototyping'],
-    bgColor: '#f6fffa',
+    title: "Cloneify",
+    description:
+      "Cloneify is a web app that allows users to listen to live air traffic control communications from around the world.",
+    image:
+      "https://framerusercontent.com/images/CWR3GLN69xNxRF87alCvZO3G9ds.jpg",
+    tags: ["UX Research", "UI Design", "Prototyping"],
+    bgColor: "#f6fffa",
     // textColor: '#101418'
   },
-]
+];
 
 export const Experience = [
   {
-    company: 'Supabase',
-    role: 'Frontend Developer',
-    date: '2022 - Present',
-    description: "Successfully developed and launched  numerous websites for Supabase, ensuring their timely delivery and overall success. Collaborated seamlessly with the design and frontend teams, collectively shaping the features and design of the Supabase Dashobard.",
+    company: "Supabase",
+    role: "Frontend Developer",
+    date: "2022 - Present",
+    description:
+      "Successfully developed and launched  numerous websites for Supabase, ensuring their timely delivery and overall success. Collaborated seamlessly with the design and frontend teams, collectively shaping the features and design of the Supabase Dashobard.",
   },
-]
+];
 
 export const Footer = [
   {
-    text: 'Linkedin',
-    url: 'https://www.linkedin.com/in/isaiah7hamilton'
+    text: "Linkedin",
+    url: "https://www.linkedin.com/in/isaiah7hamilton",
   },
   {
-    text: 'Twiter',
-    url: 'https://twitter.com/isaiah7hamilton'
+    text: "Twiter",
+    url: "https://twitter.com/isaiah7hamilton",
   },
   {
-    text: 'Github',
-    url: 'https://github.com/isaiah-hamilton'
+    text: "Github",
+    url: "https://github.com/isaiah-hamilton",
   },
-]
+];
