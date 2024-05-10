6f44f7004dc954e1b73c08a7c8205d785d27e41e
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2024-05-09 23:51:25 -0400

    update font to inter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
 import Head from 'next/head'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -25,7 +28,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:image" content="/og-image.png" />
         <title>Isaiah Hamilton | Frontend Developer</title>
       </Head>
-      <main className="bg-[#e9e8e4] text-[#101418] overflow-x-clip">
+      <main className={`bg-[#e9e8e4] text-[#101418] overflow-x-clip ${inter.className}`}>
         <Component {...pageProps} />
       </main>
     </>
